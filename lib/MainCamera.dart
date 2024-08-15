9c1c0acfd3fe0be0eba1a9623a77a7bb3781127e
Darshan Varu <darshanvaru2003@gmail.com>
2024-08-15 21:16:12 +0530

    Added MainCameraFooter widgets

--- lib/MainCamera.dart
+++ lib/MainCamera.dart
@@ -29,57 +29,80 @@ class _MainCameraState extends State<MainCamera> {
           const SizedBox(height: 10),
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 60),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Tooltip(
-                      message: _photoClicked ? 'Retake' : 'Media',
-                      child: IconButton(
-                        onPressed: !_photoClicked ? () {} : _togglePhotoClicked,
-                        icon: !_photoClicked
-                            ? const Icon(Icons.photo_library, size: 40)
-                            : const Icon(Icons.replay, size: 40),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 70,
-                  height: 70,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      _togglePhotoClicked();
-                    },
-                    shape: const CircleBorder(),
-                    foregroundColor: Colors.white,
-                    backgroundColor: _photoClicked ? Colors.red : Colors.blue,
-                    child: const Icon(Icons.camera_alt_rounded, size: 40),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 60),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Tooltip(
-                      message: _photoClicked ? 'Check' : 'Flip Camera',
-                      child: IconButton(
-                        onPressed: _photoClicked ? _togglePhotoClicked : () {},
-                        icon: _photoClicked
-                            ? const Icon(Icons.check, size: 40)
-                            : const Icon(Icons.cameraswitch_rounded, size: 40),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            child: photoClicked()
           ),
         ],
       ),
     );
+  }
+
+  Widget photoClicked() {
+    if (_photoClicked) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Tooltip(
+            message: 'Retake',
+            child: IconButton(
+              onPressed: _togglePhotoClicked,
+              icon: const Icon(Icons.replay, size: 40),
+            ),
+          ),
+          SizedBox(width: 180, height: 70),
+          Tooltip(
+            message: 'Select',
+            child: IconButton(
+              onPressed: _togglePhotoClicked, // Modify this if select action is different
+              icon: const Icon(Icons.check, size: 40),
+            ),
+          ),
+        ],
+      );
+    } else {
+      return Stack(
+        alignment: Alignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 60),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Tooltip(
+                message: 'Media',
+                child: IconButton(
+                  onPressed: () {
+                    // Handle media selection here
+                  },
+                  icon: const Icon(Icons.photo_library, size: 40),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 70,
+            height: 70,
+            child: FloatingActionButton(
+              onPressed: _togglePhotoClicked,
+              shape: const CircleBorder(),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.camera_alt_rounded, size: 40),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 60),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Tooltip(
+                message: 'Flip Camera',
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.cameraswitch_rounded, size: 40),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
   }
 }
