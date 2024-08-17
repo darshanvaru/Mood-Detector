cf9acb3e161dd4efb7b780aa5de7ee3d49b5929f
Darshan Varu <darshanvaru2003@gmail.com>
2024-08-17 10:53:32 +0530

    Changed the hierarchy of the folders

---
+++ lib/Screens/AboutUs.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../Widgets/AppDrawer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Emotion Detection',
+      debugShowCheckedModeBanner: false,
+      home: AboutUs(),
+    );
+  }
+}
+
+class AboutUs extends StatefulWidget {
+  const AboutUs({super.key});
+
+  @override
+  State<AboutUs> createState() => _AboutUsState();
+}
+
+class _AboutUsState extends State<AboutUs> {
+  final int _selectedIndex = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("About Us"),
+      ),
+      drawer: AppDrawer(selectedIndex: _selectedIndex),
+      body: const PlaceholderScreen(),
+    );
+  }
+}
+
+class PlaceholderScreen extends StatelessWidget {
+  const PlaceholderScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Placeholder Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'This is a placeholder screen',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/Guide.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../Widgets/AppDrawer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Emotion Detection',
+      debugShowCheckedModeBanner: false,
+      home: Guide(),
+    );
+  }
+}
+
+class Guide extends StatefulWidget {
+  const Guide({super.key});
+
+  @override
+  State<Guide> createState() => _GuideState();
+}
+
+class _GuideState extends State<Guide> {
+  final int _selectedIndex = 4;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Guide"),
+      ),
+      drawer: AppDrawer(selectedIndex: _selectedIndex),
+      body: const PlaceholderScreen(),
+    );
+  }
+}
+
+class PlaceholderScreen extends StatelessWidget {
+  const PlaceholderScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Placeholder Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'This is a placeholder screen',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
