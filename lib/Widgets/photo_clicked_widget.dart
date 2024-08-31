17a30e08ddb2d9428d34c64ca8f779698db7eec5
Darshan Varu <darshanvaru2003@gmail.com>
2024-08-31 17:41:45 +0530

    Photo clicked widget

--- lib/Widgets/photo_clicked_widget.dart
+++ lib/Widgets/photo_clicked_widget.dart
@@ -39,7 +39,36 @@ class PhotoClickedWidget extends StatelessWidget {
           ),
         ],
       );
-    } else {),
+    } else {
+      return Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 40.0),
+              child: Tooltip(
+                message: 'Media',
+                child: IconButton(
+                  onPressed: pickFromGallery,
+                  icon: const Icon(Icons.photo_library, size: 40),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: 70,
+              width: 70,
+              child: FloatingActionButton(
+                onPressed: capturePhoto,
+                shape: const CircleBorder(),
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                child: const Icon(Icons.camera_alt_rounded, size: 50),
+              ),
+            ),
           ),
           Align(
             alignment: Alignment.centerRight,
