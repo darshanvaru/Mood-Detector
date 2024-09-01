088e71cd58e80b5934fd1e14a1aff698fd799f57
Darshan Varu <darshanvaru2003@gmail.com>
2024-09-01 13:11:52 +0530

    added Camera_service.dart to make the code more modular

---
+++ lib/New folder/camera_service.dart
@@ -0,0 +1,26 @@
+import 'package:camera/camera.dart';
+
+class CameraService {
+  late CameraController _cameraController;
+  late Future<void> initializeControllerFuture;
+
+  CameraController get cameraController => _cameraController;
+  Future<void> get initializeFuture => initializeControllerFuture;
+
+  Future<void> initializeCamera(List<CameraDescription> cameras, int selectedIndex) async {
+    _cameraController = CameraController(
+      cameras[selectedIndex],
+      ResolutionPreset.high,
+    );
+    initializeControllerFuture = _cameraController.initialize();
+  }
+
+  Future<XFile> capturePhoto() async {
+    await initializeControllerFuture;
+    return _cameraController.takePicture();
+  }
+
+  void dispose() {
+    _cameraController.dispose();
+  }
+}
